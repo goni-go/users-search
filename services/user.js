b24045a047f2b80ee6323ee083ad7d43c06faf5a
goni <gonengoni@gmail.com>
2020-09-28 13:00:22 +0300

    update ids and countries data structures and names + remove buckets-js library

--- services/user.js
+++ services/user.js
@@ -1,19 +1,20 @@
-const { Dictionary, MultiDictionary, Set } = require("buckets-js");
 const UserRepository = require("../repositories/user");
 const TrieSearch = require("trie-search");
 const { BinarySearchTree } = require("binary-search-tree");
 const { dobToUnixTime, getMaxDoB, getMinDoB } = require("../utils/time");
 
+const MIN_LENGTH = 3;
+
 const TRIE_SERACH_OPTIONS = {
-  min: 3,
+  min: MIN_LENGTH,
   splitOnRegEx: false,
   ignoreCase: true,
 };
 
 class UserService {
   constructor() {
-    this.idsDictionary = new Dictionary();
-    this.countriesDictionary = new MultiDictionary();
+    this.idsMap = {};
+    this.countriesMap = {};
     this.namesTrieSearch = new TrieSearch("name", TRIE_SERACH_OPTIONS);
     this.agesBST = new BinarySearchTree();
     this.userRepository = new UserRepository();
@@ -21,66 +22,88 @@ class UserService {
 
   async initialize() {
     await this.userRepository.readUsers((user) => {
-      this.addIdIndex(user.id, user);
-      this.addNameIndex(user.name, user);
-      this.addCountryIndex(user.country, user);
-      this.addAgeIndex(user.dob, user);
+      this.addIdIndex(user);
+      this.addNameIndex(user);
+      this.addCountryIndex(user);
+      this.addAgeIndex(user);
     });
   }
 
-  addIdIndex(id, user) {
-    this.idsDictionary.set(id, user);
+  addIdIndex(user) {
+    this.idsMap[user.id] = user;
   }
 
-  addNameIndex(name, user) {
+  addNameIndex(user) {
     // Adding to the prefix tree the whole name
     // so we'd be able to search by full name
-    this.namesTrieSearch.add({ name: name, value: user });
+    this.namesTrieSearch.add({ name: user.name, user: user });
 
     // Adding to the prefix tree all name parts
-    // so we'd be able to search by last name or middle name
-    const nameTokens = name.split(" ");
-    for (const token of nameTokens) {
-      this.namesTrieSearch.add({ name: token, value: user });
+    // so we'd be able to search by last name and/or middle name
+    const tokens = user.name.split(" ");
+    for (let i = 1; i < tokens.length; i++) {
+      // Adding spaces to fullfild minimum of 3 chars per token
+      let token = tokens[i];
+      while (token.length < MIN_LENGTH) {
+        token += " ";
+      }
+      this.namesTrieSearch.add({ name: token, user: user });
     }
   }
 
-  addCountryIndex(country, user) {
-    this.countriesDictionary.set(country, user);
+  addCountryIndex(user) {
+    const { country, id } = user;
+    this.countriesMap[country] = this.countriesMap[country] || {};
+    this.countriesMap[country][id] = user;
   }
 
-  addAgeIndex(dob, user) {
-    this.agesBST.insert(dobToUnixTime(dob), user);
+  addAgeIndex(user) {
+    this.agesBST.insert(dobToUnixTime(user.dob), user);
   }
 
   getUser(id) {
-    return this.idsDictionary.get(id);
+    return this.idsMap[id];
   }
 
-  getUsersByName(name) {
+  getUsersByName(input) {
     // This set saves user's ids to avoid duplications
     let idsSet = new Set();
     let users = [];
+
+    // Support searching for first/middle/last name with less
+    // then minimum chars as defiened (3)
+    let name = input;
+    while (name.length < MIN_LENGTH) {
+      name += " ";
+    }
 
     // Searching in the prefix tree
     const matches = this.namesTrieSearch.get(name);
     for (const match of matches) {
       // Adding uniquely matched users that were'nt deleted
-      if (!match.value.deleted && idsSet.add(match.value.id)) {
-        users.push(match.value);
+      if (!match.user.deleted && !idsSet.has(match.user.id)) {
+        idsSet.add(match.user.id);
+        users.push(match.user);
       }
     }
 
     return users;
   }
 
-  getUsersByCountry(country) {
-    return this.countriesDictionary.get(country);
+  getUsersByCountry(input) {
+    let users = [];
+    const country = input.toUpperCase();
+    const ids = Object.keys(this.countriesMap[country] || {});
+    for (const id of ids) {
+      users.push(this.getUser(id));
+    }
+
+    return users;
   }
 
-  getUsersByAge(ageStr) {
+  getUsersByAge(input) {
     // Verify user input
-    const age = Number(ageStr);
+    const age = Number(input);
     if (isNaN(age)) {
       return [];
     }
@@ -96,7 +119,7 @@ class UserService {
   }
 
   deleteUser(id) {
-    const user = this.idsDictionary.get(id);
+    const user = this.idsMap[id];
     if (!user) {
       return false;
     }
@@ -105,9 +128,9 @@ class UserService {
     // support deletion
     user.deleted = true;
 
-    this.countriesDictionary.remove(user.country, id);
+    delete this.countriesMap[user.country][user.id];
+    delete this.idsMap[user.id];
     this.agesBST.delete(dobToUnixTime(user.dob), id);
-    this.idsDictionary.remove(id);
 
     // Deleting user from filesystem
     this.userRepository.deleteUser(id);
